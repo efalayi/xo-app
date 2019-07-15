b6c23a8daf63b2794598e6223412787515729fe7
Esther Falayi <esther.falayi@andela.com>
2019-07-15 12:26:59 +0100

    chore(setup): initialise app with expo

---
+++ constants/Colors.js
@@ -0,0 +1,27 @@
+const YELLOW = '#F2BC1B';
+const YELLOW_DARK = '#F2A413';
+const ORANGE = '#D96B0B';
+const ORANGE_DARK = '#BF4904';
+const BLACK = '#0D0D0D';
+const WHITE = '#FFF';
+
+// const tintColor = '#2f95dc';
+const tintColor = YELLOW;
+
+export default {
+  black: BLACK,
+  errorBackground: 'red',
+  errorText: WHITE,
+  noticeBackground: tintColor,
+  noticeText: WHITE,
+  orange: ORANGE,
+  orangeDark: ORANGE_DARK,
+  tintColor,
+  tabIconDefault: '#CCC',
+  tabIconSelected: tintColor,
+  tabBar: '#FEFEFE',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  yellow: YELLOW,
+  yellowDark: YELLOW_DARK
+};
